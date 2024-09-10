4f20069932e82eb72a9e99c0563c9b8ca47e1f8e
Carina Ogbat <carinawrightogbat@gmail.com>
2024-09-09 20:27:04 -0700

    completed navigation

--- nextjs-blog/lib/posts.js
+++ nextjs-blog/lib/posts.js
@@ -1,6 +1,8 @@
 import fs from 'fs';
 import path from 'path';
 import matter from 'gray-matter';
+import { remark } from 'remark';
+import html from 'remark-html';
 
 const postsDirectory = path.join(process.cwd(), 'posts');
 
@@ -35,6 +37,54 @@ export function getSortedPostsData() {
   });
 }
 
+export function getAllPostIds() {
+    const fileNames = fs.readdirSync(postsDirectory);
+  
+    // Returns an array that looks like this:
+    // [
+    //   {
+    //     params: {
+    //       id: 'ssg-ssr'
+    //     }
+    //   },
+    //   {
+    //     params: {
+    //       id: 'pre-rendering'
+    //     }
+    //   }
+    // ]
+
+    return fileNames.map((fileName) => {
+      return {
+        params: {
+          id: fileName.replace(/\.md$/, ''),
+        },
+      };
+    });
+  }
+
+  export async function getPostData(id) {
+    const fullPath = path.join(postsDirectory, `${id}.md`);
+    const fileContents = fs.readFileSync(fullPath, 'utf8');
+  
+    // Use gray-matter to parse the post metadata section
+    const matterResult = matter(fileContents);
+  
+    // Use remark to convert markdown into HTML string
+    const processedContent = await remark()
+      .use(html)
+      .process(matterResult.content);
+    const contentHtml = processedContent.toString();
+  
+    // Combine the data with the id and contentHtml
+    return {
+      id,
+      contentHtml,
+      ...matterResult.data,
+    };
+  }
+
+
 // ***fetch data from an API***
 // export async function getSortedPostsData() {
 //     // Instead of the file system,
@@ -42,6 +92,8 @@ export function getSortedPostsData() {
 //     const res = await fetch('..');
 //     return res.json();
 //   }
+
+
 
 // *** query a database directly ***
 // import someDatabaseSDK from 'someDatabaseSDK'
@@ -53,6 +105,8 @@ export function getSortedPostsData() {
 //   // fetch post data from a database
 //   return databaseClient.query('SELECT posts...')
 // }
+
+
 
 // *** getting server side props ***
 // export async function getServerSideProps(context) {
